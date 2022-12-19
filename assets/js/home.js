5a86c0ec4e59f99493b523e17f0e552473ccfb82
Martindvttt <114313338+Martindvttt@users.noreply.github.com>
2022-12-19 16:17:50 +0100

    Martin

---
+++ assets/js/home.js
@@ -0,0 +1,24 @@
+// Initialize map
+var map = L.map('map', {
+    center: [0, 0],
+    zoom: 2
+});
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Update map on scroll
+window.addEventListener('scroll', function () {
+    // Get current scroll position
+    var scrollPos = window.pageYOffset;
+
+    // Calculate map center based on scroll position
+    var center = map.getCenter();
+    center.lat = center.lat + scrollPos / 100;
+    center.lng = center.lng + scrollPos / 100;
+
+    // Update map center
+    map.setView(center);
+});
---
+++ assets/js/onboarding.js
@@ -0,0 +1,51 @@
+// Récupère le conteneur de l'onboarding, les points d'onboarding et les étapes
+const onboardingContainer = document.querySelector('.onboarding_container');
+const onboardingDots = document.querySelectorAll('.onboarding_dot');
+const steps = document.querySelectorAll('.step');
+
+// Récupère les boutons "Suivant"
+const nextButtons = document.querySelectorAll('.onboarding_next_btn');
+const secondNextButton = document.querySelector('.onboarding_second_next_btn');
+
+// Récupère le lien "passer"
+const skipLink = document.querySelector('.step a');
+
+// Affiche le premier écran d'onboarding
+onboardingContainer.classList.add('active');
+onboardingDots[0].classList.add('active');
+steps[0].classList.add('active');
+
+// Ajoute un écouteur d'événements sur les boutons "Suivant" pour passer à l'étape suivante
+nextButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // Récupère l'index de l'étape active
+        const activeIndex = [...steps].findIndex((step) => step.classList.contains('active'));
+
+        // Masque l'étape active
+        steps[activeIndex].classList.remove('active');
+        onboardingDots[activeIndex].classList.remove('active');
+
+        // Affiche l'étape suivante
+        steps[activeIndex + 1].classList.add('active');
+        onboardingDots[activeIndex + 1].classList.add('active');
+    });
+});
+
+// Ajoute un écouteur d'événements sur le bouton "Suivant" du dernier écran pour masquer l'écran d'onboarding
+secondNextButton.addEventListener('click', () => {
+    onboardingContainer.classList.remove('active');
+});
+
+// Ajoute un écouteur d'événements sur le lien "passer" pour passer à l'étape suivante
+skipLink.addEventListener('click', () => {
+    // Récupère l'index de l'étape active
+    const activeIndex = [...steps].findIndex((step) => step.classList.contains('active'));
+
+    // Masque l'étape active
+    steps[activeIndex].classList.remove('active');
+    onboardingDots[activeIndex].classList.remove('active');
+
+    // Affiche l'étape suivante
+    steps[activeIndex + 1].classList.add('active');
+    onboardingDots[activeIndex + 1].classList.add('active');
+});
